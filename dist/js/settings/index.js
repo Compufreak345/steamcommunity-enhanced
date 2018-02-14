acdc161e0161dd1256d28152350cb2c08e94113c
Compufreak345 <Compufreak345@gmail.com>
2018-02-14 21:49:10 +0100

    Jquery is finally working :smile_cat:

--- dist/js/settings/index.js
+++ dist/js/settings/index.js
@@ -1,9 +1,3 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var $ = require("jquery");
-console.log("Hi");
-$(document).ready(function () {
-    console.log("Ho");
+$(function () {
     $("#global_header div.supernav_container").append("<a class='menuitem supernav'>Enhanced</a>");
 });
-console.log("Hey");
--- src/settings/index.ts
+++ src/settings/index.ts
@@ -1,9 +1,4 @@
-import $ = require("jquery")
-console.log("Hi") 
-$(document).ready(function(){
-    
-console.log("Ho");
-$("#global_header div.supernav_container").append("<a class='menuitem supernav'>Enhanced</a>");
-});
 
-console.log("Hey") 
+$(function(){ 
+    $("#global_header div.supernav_container").append("<a class='menuitem supernav'>Enhanced</a>");
+});
